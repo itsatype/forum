338a55ae3ac42fb621e844dda69161b03dfbab78
Leah Einhorn <leaheinhorn@Leahs-Air.home>
2016-10-16 12:48:41 -0400

    add seed and style comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(name: "Leahein", email: "leahein@gmail.com", password: "password")
+Post.create(title: "I was thinking...", content: "Thinx is awesome", user: user)
